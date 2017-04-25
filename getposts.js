e9f386844483cd0625bcbfe9d660da1b29f771bc
JTR <riddick.taylor@gmail.com>
2017-04-24 22:31:43 -0500

    saving and retrieving of posts nominally working

--- getposts.js
+++ getposts.js
@@ -1,9 +1,22 @@
 function getPosts(app) {
   var fs = require('fs');
+  var entries = []
 
   var postsFile = fs.readFile('static/posts.csv','utf-8',function(err,posts){
     var posts = posts.split('\n');
-  })
+    for (var i=1;i<posts.length;i++){
+      let row = posts[i].split(',');
+      let entry = {
+        "title": row[0],
+        "content": row[1],
+        "published": row[2]
+      };
+      if (entry.title !== ""){
+        entries.push(entry);
+      }
+    };
+    app.locals.entries = entries;
+  });
 
 }
 
